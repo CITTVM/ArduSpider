b5fa2742f06714fb89229c5c3dc07ae01bcf2a04
JordanSilva <jordan.silva@hotmail.es>
2017-05-02 13:25:14 -0300

    Fix Ultrasonico y Giro

--- test/test.ino
+++ test/test.ino
@@ -6,6 +6,7 @@ Servo gfi, gmi, gfd, gmd, gtd, gti;
 Servo bfi,bfd,bmd,bmi, btd, bti;
 
 
+
 void setup() {
    
  gfi.attach(1);
@@ -25,10 +26,16 @@ void setup() {
 }
 
 
-void loop() {  
- if (ultrasonic.Ranging(CM)>5){
+void loop() {
+  int i=0;  
+ if (ultrasonic.Ranging(CM)>15){
   caminar();
+ }else{
+  for(i=0;i<7;i++){
+    girarIzquiera();
+  }
  }
+ delay(200);
 }
 
 
@@ -39,50 +46,105 @@ void caminar(){
 //subir  izquierdo
  bfi.write(115);
  bmd.write(75);
- bti.write(70);
- delay(500);
+ bti.write(40);
+ delay(200);
 
  //avance izquierdo
  gfi.write(60);
   gmd.write(125);
   gti.write(80);
-  delay(500);
+  delay(200);
 
 //bajar izquierdo
  bfi.write(95);
  bmd.write(90);
- bti.write(90);
- delay(500);
+ bti.write(65);
+ delay(200);
  
   //retroceso izquierdo
  gfi.write(100);
   gmd.write(90);
   gti.write(130);
-  delay(500);
+  delay(200);
 
   //subir Derecha
   bfd.write(60);
   bmi.write(120);
   btd.write(110);
-  delay(500);
+  delay(200);
 
 //avance Derecha
   gfd.write(125);
   gmi.write(60);
   gtd.write(90);
-  delay(500);
+  delay(200);
 
   //bajar Derecha
   bfd.write(90);
   bmi.write(90);
   btd.write(90);
-  delay(500);
+  delay(200);
 
   //retroceso Derecha
   gfd.write(90);
   gmi.write(85);
   gtd.write(50);
-  delay(500);
+  delay(200);
+  
+}
+
+void girarIzquiera(){
+
+ //subir Derecha
+  bfd.write(60);
+  bmi.write(120);
+  btd.write(110);
+  delay(200);
+
+//avance Derecha
+  gfd.write(125);
+  gmi.write(60);
+  gtd.write(90);
+  delay(200);
+
+  //bajar Derecha
+  bfd.write(90);
+  bmi.write(90);
+  btd.write(90);
+  delay(200);
+
+  //retroceso Derecha
+  gfd.write(90);
+  gmi.write(85);
+  gtd.write(50);
+  delay(200);
+
+   
+//subir  izquierdo
+ bfi.write(115);
+ bmd.write(75);
+ bti.write(40);
+ delay(200);
+
+  //retroceso izquierdo
+ gfi.write(100);
+  gmd.write(90);
+  gti.write(130);
+  delay(200);
+
+//bajar izquierdo
+ bfi.write(95);
+ bmd.write(90);
+ bti.write(65);
+ delay(200);
+ 
+ 
+  //avance izquierdo
+ gfi.write(60);
+  gmd.write(125);
+  gti.write(80);
+  delay(200);
+
   
 }
 
